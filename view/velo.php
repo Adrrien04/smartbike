84fd1912a69a0e9bd70c197c9e606a8dff73d013
Adrrien04 <adrrien.chandrakumar@gmail.com>
2023-12-19 09:56:29 +0100

    mise en grid

--- view/velo.php
+++ view/velo.php
@@ -5,21 +5,47 @@ include_once('model/bdd.php');
 <html>
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <style>
+        /* Ajoutez un peu de style pour les grilles */
+        .velo-grid {
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+            gap: 20px;
+        }
+
+        .velo-card {
+            border: 1px solid #ddd;
+            padding: 15px;
+            text-align: center;
+        }
+
+        .velo-image {
+            max-width: 100%;
+            height: auto;
+            margin-bottom: 10px;
+        }
+    </style>
 </head>
 <body>
 <h1>Nos Vélos</h1>
 
 <?php
 if (is_array($velos) && count($velos) > 0) {
+    echo '<div class="velo-grid">';
+
     foreach ($velos as $velo) {
         $photo_url = isset($velo['photo_url']) ? $velo['photo_url'] : 'chemin/par/defaut.jpg';
 
-        echo '<p>Image : <img src="' . $photo_url . '" class="img-fluid" alt="Vélo"></p>';
-        echo '<p>Nom du vélo : ' . $velo['titre'] . '</p>';
+        echo '<div class="velo-card">';
+        echo '<img src="' . $photo_url . '" class="velo-image" alt="Vélo">';
+        echo '<p>' . $velo['titre'] . '</p>';
         echo '<p>Description : ' . $velo['description'] . '</p>';
         echo '<p>Prix : ' . $velo['prix'] . ' €</p>';
         echo '<hr>';
+        echo '</div>';
     }
+
+    echo '</div>';
 } else {
     echo '<p>Aucun vélo disponible pour le moment.</p>';
 }
